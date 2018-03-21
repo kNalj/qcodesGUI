6f2140d805eee9af5350b2c52f0bd3337e7c440c
nanoelectronics-new <josip.kukucka@ist.ac.at>
2018-03-21 16:21:12 +0100

    Added displaying of station instruments in main window

--- AddInstrumentWidget.py
+++ AddInstrumentWidget.py
@@ -1,16 +1,19 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QLineEdit, QPushButton, QLabel, QComboBox, QVBoxLayout, QRadioButton
+from PyQt5.QtWidgets import QApplication, QWidget, QLineEdit, QPushButton, QLabel, QComboBox, QVBoxLayout, QRadioButton, QMessageBox
 from PyQt5 import QtGui
 import sys
 
 from qcodes.tests.instrument_mocks import DummyInstrument
 
+
 class Widget(QWidget):
     def __init__(self, instruments, parent=None):
-        super(Widget, self).__init__(parent)
+        super(Widget, self).__init__()
 
         self.instruments = instruments
-        self.premade_instruments = {"Dummy instrument" : DummyInstrument, "Other premade instrument" : "Some other class", "Yet another instrument" : "Third instruments class"}
-
+        self.premade_instruments = {"Dummy instrument": DummyInstrument,
+                                    "Other premade instrument": "Some other class",
+                                    "Yet another instrument": "Third instruments class"}
+        self.parent = parent
         self.init_ui()
         self.show()
 
@@ -21,7 +24,7 @@ class Widget(QWidget):
         self.setWindowIcon(QtGui.QIcon("osciloscope_icon.png"))
         
         self.cb = QComboBox(self)
-        self.cb.move(20,20)
+        self.cb.move(20, 20)
         self.cb.resize(280, 30)
         for key, value in self.premade_instruments.items():
             self.cb.addItem(key)
@@ -30,6 +33,7 @@ class Widget(QWidget):
         self.label = QLabel("Type", self)
         self.label.move(20, 65)
         self.instrument_type = QLineEdit(self)
+        #self.instrument_type.setText()
         self.instrument_type.move(20, 80)
         self.instrument_type.setEnabled(False)
         
@@ -61,23 +65,61 @@ class Widget(QWidget):
         self.update_instrument_data()
 
     def add_instrument(self):
-        '''insert logic here'''
+        """
+        Called upon clicking OK. Adds a new instrument based on input by user to the instrument dictionary in main window.
+        Data structure -> instruments[name] : [instrument object, sweep/measure, gate to sweep/observe]
+        """
+
+        if self.validate_instrument_input():
+            return
+                
         instrument = self.premade_instruments[self.instrument_type.text()]
         gates = self.instrument_gates.text().split(",")
         name = self.instrument_name.text()
-        #print(gates)
-        if self.b1.isChecked(): # sweep
-            self.instruments[name] = [instrument(name, gates=gates), "sweep", "g1"]
-        elif self.b2.isChecked():
-            self.instruments[name] = [instrument(name, gates=gates), "measure", "g1"]
-
+        gate = self.observed_gate.text()
         
+        if self.b1.isChecked(): # sweep
+            self.instruments[name] = [instrument(name, gates=gates), "sweep", gate]
+        elif self.b2.isChecked():
+            self.instruments[name] = [instrument(name, gates=gates), "measure", gate]
+
+        self.parent.update_station_preview()
         self.close()
         
     def update_instrument_data(self):
         
         self.instrument_type.setText(self.cb.currentText())
         self.instrument_gates.setText("g1,g2,g3")
+        
+    def show_error_message(self, title, message):
+        msg_box = QMessageBox()
+        msg_box.setIcon(QtGui.QMessageBox.Warning)
+        msg_box.setWindowIcon(QtGui.QIcon("warning_icon.png"))
+        msg_box.setWindowTitle(title)
+        msg_box.setText(message)
+        msg_box.setStandardButtons(QtGui.QMessageBox.Ok)
+        msg_box.exec_()
+        
+    def validate_instrument_input(self):
+        if len(self.instrument_name.text()) < 1:
+            #should also check is there is an instrument with a same name
+            error_message = "Please specify instrument name."
+        elif len(self.instrument_gates.text()) < 1:
+            #validate with REGEX maaaaybe ?
+            error_message = "Please specify instrument gates."
+        elif len(self.observed_gate.text()) < 1:
+            #should add check if specified gate is withing gates
+            error_message = "Please specify observed gate."
+        elif self.b1.isChecked() == False and self.b2.isChecked() == False:
+            error_message = "Please specify instrument action (sweep/measure)"
+        else:
+            error_message = ""
+        
+        if error_message != "":
+            self.show_error_message("Warning", error_message)
+            return True
+        else:
+            return False
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
--- qcodesMainWindow.py
+++ qcodesMainWindow.py
@@ -7,6 +7,7 @@ import sys
 import qcodes as qc
 from qcodes.tests.instrument_mocks import DummyInstrument
 
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -15,6 +16,7 @@ class MainWindow(QMainWindow):
         self.init_menu_bar()
 
         self.instruments = {}
+        self.station_instruments = {}
 
         self.statusBar().showMessage("Ready")
         self.show()
@@ -32,15 +34,15 @@ class MainWindow(QMainWindow):
         labels = ["Lower limit", "Upper limit", "Num", "Step"]
         first_location = [40, 80]
         
-        self.label = QLabel("Parameters", self)
-        self.label.move(25,  25)
+        label = QLabel("Parameters", self)
+        label.move(25,  25)
         
         for i in range(len(labels)):
-            self.label = QLabel(labels[i], self)
-            self.label.move(first_location[0] + i*150, first_location[1]-25)
+            label = QLabel(labels[i], self)
+            label.move(first_location[0] + i*150, first_location[1]-25)
         
         self.textbox_lower_limit = QLineEdit(self)
-        self.textbox_lower_limit.move(first_location[0],first_location[1])
+        self.textbox_lower_limit.move(first_location[0], first_location[1])
         self.textbox_lower_limit.resize(130, 30)
 
         self.textbox_upper_limit = QLineEdit(self)
@@ -55,21 +57,21 @@ class MainWindow(QMainWindow):
         self.textbox_step.move(490, 80)
         self.textbox_step.resize(130, 30)
         
-        self.label = QLabel("Instruments", self)
-        self.label.move(25,  130)
+        label = QLabel("Instruments:", self)
+        label.move(25,  130)
         
         self.btn_add_instrument = QPushButton("Add instrument", self)
-        self.btn_add_instrument.move(490,150)
+        self.btn_add_instrument.move(490, 150)
         self.btn_add_instrument.resize(140, 40)
         self.btn_add_instrument.clicked.connect(self.add_new_instrument)
         
         self.btn_run = QPushButton("Run", self)
-        self.btn_run.move(560,320)
+        self.btn_run.move(560, 320)
         self.btn_run.resize(60, 40)
         self.btn_run.clicked.connect(self.run_qcodes)
         
         self.btn_show_station = QPushButton("Show station instruments", self)
-        self.btn_show_station.move(560,260)
+        self.btn_show_station.move(560, 260)
         self.btn_show_station.resize(60, 40)
         self.btn_show_station.clicked.connect(self.show_station)
 
@@ -110,12 +112,12 @@ class MainWindow(QMainWindow):
         Opens a new Widget (window) with text inputs for parameters of an instrument, creates new instrument(s)
         :return: NoneType
         """
-        self.add_instrument = Widget(self.instruments)
+        self.add_instrument = Widget(self.instruments, parent=self)
         self.add_instrument.show()
         
     def run_qcodes(self):
         
-        #should enclose into try-catch
+        # should enclose into try-catch
         self.lower = float(self.textbox_lower_limit.text()) if self.textbox_lower_limit.text() != "" else 0
         self.upper = float(self.textbox_upper_limit.text()) if self.textbox_upper_limit.text() != "" else 1
         self.num = float(self.textbox_num.text()) if self.textbox_num.text() != "" else 10
@@ -123,16 +125,24 @@ class MainWindow(QMainWindow):
         
         station = qc.Station()
         for key, value in self.instruments.items():
-            station.add_component(value[0], key)
-            if value[1] == "sweep":
-                sweep_parameter  = self.instruments[key][0].parameters[value[2]]
-            elif value[1] == "measure":
-                measure_parameter = self.instruments[key][0].parameters[value[2]]
-        
-        #look into this dmm.v1 (how the hell is this v1 created, some dynamic magical monstrosity)
-        #Found it at InstrumentBase.parameters -> dictionary with parameter string as keys
-        #C:\Users\nanoelectronics\Anaconda3\envs\qcodes\lib\site-packages\qcodes\instrument\base.py -> line 53
-        #lp = qc.Loop(dac.ch1.sweep(self.lower, self.upper, num=self.num), self.step).each(dmm.v1)
+            
+            name = key
+            instrument = value[0]
+            action = value[1]
+            parameter = value[2]
+            
+            station.add_component(instrument, name)
+            if action == "sweep":
+                sweep_parameter = instrument.parameters[parameter]
+            elif action == "measure":
+                measure_parameter = instrument.parameters[parameter]
+
+        # look into this dmm.v1 (how the hell is this v1 created, some dynamic magical monstrosity)
+        # Found it at InstrumentBase.parameters -> dictionary with parameter string as keys
+        # C:\Users\nanoelectronics\Anaconda3\envs\qcodes\lib\site-packages\qcodes\instrument\base.py -> line 53
+        # lp = qc.Loop(dac.ch1.sweep(self.lower, self.upper, num=self.num), self.step).each(dmm.v1)
+
+        # make sure both required parameters exist before running a loop
         lp = qc.Loop(sweep_parameter.sweep(self.lower, self.upper, num=self.num), self.step).each(measure_parameter)
         data = lp.run('data/dataset')
         
@@ -140,13 +150,36 @@ class MainWindow(QMainWindow):
         for key, value in self.instruments.items():
             print(key)
             print(value[0].parameters)
-        
-    
-#pprint(station.snapshot())
-#print(data.snapshot())
+
+        print("Station instruments")
+        for key, value in self.station_instruments.items():
+            print(key)
+            print(value[0].parameters)
+
+    def update_station_preview(self):
+        if len(self.instruments) == 1:
+            header_string = "Nr." + "       " + "Name" + "      " + "Type"
+            new_label = QLabel(header_string, self)
+            new_label.move(35, 150)
+            new_label.resize(300, 20)
+            new_label.show()
+
+        for instrument in self.instruments:
+            if instrument not in self.station_instruments:
+                display_string = str((len(self.instruments))) + ".      " + instrument + "      " + str(self.instruments[instrument][0])
+                new_label = QLabel(display_string, self)
+                new_label.move(35, 170 + 20*len(self.station_instruments))
+                new_label.resize(300, 20)
+                new_label.show()
+
+                self.station_instruments[instrument] = self.instruments[instrument]
 
 
-if __name__ == '__main__':
+def main():
     app = QApplication(sys.argv)
     ex = MainWindow()
     sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
