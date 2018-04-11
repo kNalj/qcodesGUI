fa5125853ec7a1204d7b1e6c1caafa8b445c437f
nanoelectronics-new <josip.kukucka@ist.ac.at>
2018-04-11 17:10:35 +0200

    Added function descriptions to some new functions

--- InstrumentData.py
+++ InstrumentData.py
@@ -20,5 +20,6 @@ EXAMPLE:
 # this particular data is for the computer in the lab closest to the exit of the lab
 instrument_data = {
 "IVVI": ["dac", "ASRL5::INSTR"],
-"Agilent_34400A":  ["dmm", "USB0::0x2A8D::0x0101::MY54505177::INSTR"]
+"Agilent_34400A":  ["dmm", "USB0::0x2A8D::0x0101::MY54505177::INSTR"],
+"Agilent_34450A": ["dmm", "USB0::0x2A8D::0xB318::MY58020037::INSTR"]
 }
